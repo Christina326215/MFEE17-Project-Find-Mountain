c3d9f8d012f6553d05d27436fe8601bc81d57957
tzulingtseng <y3y6xu4@cengzilingde-MacBook-Air.local>
2021-09-11 14:28:48 +0800

    新增明信片背景 & 穿搭組合版

--- mountain-react/src/App.js
+++ mountain-react/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 //====== below components star ======//
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Outfit from './components/OutfitPage/Outfit'
 //====== above components end ======//
 
 function App() {
@@ -24,7 +25,7 @@ function App() {
           <Route path="/shop/cart">
             {/* 放要引入的content EX: <Stock /> */}
           </Route>
-          <Route path="/outfit">{/* 放要引入的content EX: <Stock /> */}</Route>
+          <Route path="/outfit"><Outfit/></Route>
           <Route path="/login">{/* 放要引入的content EX: <Stock /> */}</Route>
           <Route path="/member">{/* 放要引入的content EX: <Stock /> */}</Route>
           {/* //===homepage 路由放最下面===// */}
@@ -32,36 +33,6 @@ function App() {
             {/* 放要引入的content EX: <Stock /> */}
           </Route>
         </Switch>
-
-        {/* need delete fake content star */}
-        <div>
-          <h1>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-            veniam natus ratione similique autem, inventore facere minus quidem,
-            voluptate id voluptatem repellendus pariatur maxime, tempore quod
-            magnam officiis aut incidunt.
-          </h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-            nihil totam laborum, aspernatur magnam vero non! Illum similique,
-            sunt iste dolorem, ipsa corrupti consequuntur asperiores possimus
-            rerum ut voluptates inventore. Tempora temporibus sequi deleniti
-            dolore ducimus provident, obcaecati quasi quam nesciunt. Ex eaque
-            illum nihil nostrum quas error eveniet nobis enim? Nam iste eligendi
-            laudantium. Totam illo enim mollitia esse! Labore aspernatur eum,
-            ipsa ea porro quod eius non et soluta officiis? Nemo in consequatur
-            totam optio dolorum, incidunt accusantium est eligendi, repellat
-            dignissimos earum eos explicabo distinctio sint aperiam? Nisi
-            ratione laudantium reprehenderit neque quos explicabo iste dicta?
-            Doloremque tempore ad magnam accusamus. Fugit esse accusamus
-            veritatis ab. Quis itaque culpa tenetur est cum vel beatae delectus
-            eos totam! Dolore quos dignissimos minus ipsum distinctio fugiat non
-            doloremque laborum nobis aspernatur enim magnam quidem laboriosam,
-            reiciendis provident nisi error, nam eaque molestias! Quibusdam
-            dolores quisquam similique vel autem nostrum!
-          </p>
-        </div>
-        {/* need delete fake content end */}
 
         <Footer />
       </>
