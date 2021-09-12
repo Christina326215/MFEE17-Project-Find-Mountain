7cd41dd5c06b191013c12a094055d9a276a2937c
tzulingtseng <y3y6xu4@cengzilingde-MacBook-Air.local>
2021-09-12 21:03:14 +0800

    add ScrollToTop

--- mountain-react/src/App.js
+++ mountain-react/src/App.js
@@ -15,6 +15,7 @@ import MapL from './components/MapPage/MapL';
 import MapM from './components/MapPage/MapM';
 import MapH from './components/MapPage/MapH';
 import Outfit from './components/OutfitPage/Outfit';
+import ScrollToTop from './components/ScrollToTop';
 
 //====== above components end ======//
 
@@ -25,52 +26,55 @@ function App() {
     <Router>
       <>
         <Navbar auth={auth} setAuth={setAuth} />
-
-        <Switch>
-          {/* //===other 路由放homepage上面 */}
-          <Route path="/recommend/bear/result">
-            <Result />
-          </Route>
-          <Route path="/recommend/bear">
-            <Bear />
-          </Route>
-          <Route path="/recommend/manual">
-            <Manual />
-          </Route>
-          <Route path="/recommend/detail">
-            <Detail />
-          </Route>
-          <Route path="/map/levelH">
-            <MapH />
-          </Route>
-          <Route path="/map/levelM">
-            <MapM />
-          </Route>
-          <Route path="/map">
-            <MapL />
-          </Route>
-          <Route path="/recommend">
-            <Recommend />
-          </Route>
-          <Route path="/shop/bags">
-            <Bags />
-          </Route>
-          <Route path="/shop">
-            <ShopMain />
-          </Route>
-          <Route path="/shop/cart">
-            {/* 放要引入的content EX: <Stock /> */}
-          </Route>
-          <Route path="/outfit">
-            <Outfit />
-          </Route>
-          <Route path="/login">{/* 放要引入的content EX: <Stock /> */}</Route>
-          <Route path="/member">{/* 放要引入的content EX: <Stock /> */}</Route>
-          {/* //===homepage 路由放最下面===// */}
-          <Route exact path="/">
-            {/* 放要引入的content EX: <Stock /> */}
-          </Route>
-        </Switch>
+        <ScrollToTop>
+          <Switch>
+            {/* //===other 路由放homepage上面 */}
+            <Route path="/recommend/bear/result">
+              <Result />
+            </Route>
+            <Route path="/recommend/bear">
+              <Bear />
+            </Route>
+            <Route path="/recommend/manual">
+              <Manual />
+            </Route>
+            <Route path="/recommend/detail">
+              <Detail />
+            </Route>
+            <Route path="/map/levelH">
+              <MapH />
+            </Route>
+            <Route path="/map/levelM">
+              <MapM />
+            </Route>
+            <Route path="/map">
+              <MapL />
+            </Route>
+            <Route path="/recommend">
+              <Recommend />
+            </Route>
+            <Route path="/shop/bags">
+              <Bags />
+            </Route>
+            <Route path="/shop">
+              <ShopMain />
+            </Route>
+            <Route path="/shop/cart">
+              {/* 放要引入的content EX: <Stock /> */}
+            </Route>
+            <Route path="/outfit">
+              <Outfit />
+            </Route>
+            <Route path="/login">{/* 放要引入的content EX: <Stock /> */}</Route>
+            <Route path="/member">
+              {/* 放要引入的content EX: <Stock /> */}
+            </Route>
+            {/* //===homepage 路由放最下面===// */}
+            <Route exact path="/">
+              {/* 放要引入的content EX: <Stock /> */}
+            </Route>
+          </Switch>
+        </ScrollToTop>
 
         <Footer />
       </>
