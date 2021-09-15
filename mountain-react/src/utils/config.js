0df538deab9e3c831f4941411ce09bb6114a4ad6
ouyangyuro <def2446@gmail.com>
2021-09-15 10:06:16 +0800

    9/15 renew & add .env to gitignore

--- mountain-react/src/utils/config.js
+++ mountain-react/src/utils/config.js
@@ -1,5 +1,2 @@
-export const stockURL =
-  process.env.REACT_APP_stockURL || 'http://localhost:3500/api/stock/';
-
-export const authURL =
-  process.env.REACT_APP_authURL || 'http://localhost:3500/api/auth/';
+export const mapURL =
+  process.env.REACT_APP_mapURL || 'http://localhost:3500/api/map/';
