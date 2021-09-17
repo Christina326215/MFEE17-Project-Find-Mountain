0768e6141dd2547050f4db6e29cd7ab5842f6c1c
abbychen0427 <abbychen0427@gmail.com>
2021-09-17 11:27:42 +0800

    新增router下產品分類頁

--- mountain-express/routers/shopPage.js
+++ mountain-express/routers/shopPage.js
@@ -2,8 +2,23 @@ const express = require("express");
 const router = express.Router();
 const connection = require("../utils/db");
 
-router.get("", async function (req, res, next) {
+router.get("/", async function (req, res, next) {
   let dbResults = await connection.queryAsync("SELECT * FROM product"); // 等資料庫查詢資料
+  res.json(dbResults);
+});
+
+router.get("/shoes", async function (req, res, next) {
+  let dbResults = await connection.queryAsync("SELECT * FROM product WHERE type = 1"); // 等資料庫查詢資料
+  res.json(dbResults);
+});
+
+router.get("/bags", async function (req, res, next) {
+  let dbResults = await connection.queryAsync("SELECT * FROM product WHERE type = 2"); // 等資料庫查詢資料
+  res.json(dbResults);
+});
+
+router.get("/clothes", async function (req, res, next) {
+  let dbResults = await connection.queryAsync("SELECT * FROM product WHERE type = 3"); // 等資料庫查詢資料
   res.json(dbResults);
 });
 
