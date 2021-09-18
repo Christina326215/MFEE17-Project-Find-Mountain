e426f66d4e99015d4ebf59f89c8150d00713dc4d
Christina326215 <christina326215@gmail.com>
2021-09-18 23:42:31 +0800

    完成會員資料及購物車之地址 郵遞區號連動縣市及行政區

--- mountain-react/src/components/MemberPage/MemberEdit copy.js
+++ mountain-react/src/components/MemberPage/MemberEdit copy.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'; //a標籤要變成link
 import { withRouter } from 'react-router-dom'; //可以獲取history,location,match,來使用
 import $ from 'jquery';
 import '../../styles/MemberPage/MemberPersonal.scss'; //member product and article style
-import { useAuth } from '../../context/auth';
-import { zipCodeURL, zipGroupURL } from '../../utils/config';
+import { useAuth } from '../../context/auth'; // 取得會員資料
+import { zipCodeURL, zipGroupURL } from '../../utils/config'; // 取得郵遞區號資料
 import axios from 'axios';
 
 //====== below pages star ======//
@@ -12,42 +12,96 @@ import MemberSideHead from './pages/MemberSideHead'; //member Side Head
 //====== below pages end ======//
 
 function MemberEdit() {
+  // 1. 首先，建立好 html 在 return(<>...</>)。
+  // 2. 設定狀態，關於共用會員資料使用useAuth()，關於地址資料放在靜態檔案中則使用useState()。
   const { member } = useAuth();
   const [zipGroup, setZipGroup] = useState(null);
-  const [cities, setCities] = useState([]);
+  const [zipCode, setZipCode] = useState(null);
+  const [cities, setCities] = useState([]); // 各縣市陣列
+  const [city, setCity] = useState(null); // 選擇的唯一縣市
+  const [districts, setDistricts] = useState([]); //各行政區陣列
+  const [codes, setCodes] = useState([]);
 
-  //  當member改變的時候，要去setTempMember
-
+  // 3. 因為不能直接去改動member的資料，需要先設定一個tempMember變數，將由資料庫而來的member放進setTempMember中改變狀態，最後才會把改變後的狀態存進資料庫。
   const [tempMember, setTempMember] = useState(null);
+  // 4. 當member有資料時，就會使用useEffect，以setTempMember改變狀態(原為null)。
   useEffect(() => {
     if (member !== null) {
       setTempMember({ ...member });
     }
   }, [member]);
 
+  // 5. 填入原始member資料後，當Html input欄位有輸入變動時，onChange呼叫handleChange函式，將react的變數tempMember，轉換成html上偵測的變數與其值([e.target.name]: e.target.value)，其中[e.target.name]為key。
   function handleChange(e) {
     // console.log(e.target.name, e.target.value);
+    // console.log('onChange', e.target.name, e.target.value);
     setTempMember({ ...tempMember, [e.target.name]: e.target.value });
+    // console.log('onChange After');
   }
 
   useEffect(() => {
-    // 從靜態檔案抓資料
+    // console.log('useEffect for tempMember');
+  }, [tempMember]);
+
+  // 還沒input之前，出現兩次useEffect for tempMember，結果如下：
+  // useEffect for tempMember
+  // useEffect for tempMember
+  // 是因為第一次render原本是空的，第二次render才有東西。
+
+  // 輸入input之後(使input欄位有變動)，結果如下：
+  // onChange name 王大明1
+  // onChange After
+  // useEffect for tempMember
+  // 因為setXXX是非同步且Single Thread，所以會把setTempMember丟給quere，先繼續下一行console.log('useEffect for tempMember');
+
+  // 6. 從靜態檔案抓資料，取得 group.json 與 code.json 地址相關資料。
+  useEffect(() => {
     async function getZipGroup() {
       try {
         const zipGroupRes = await axios.get(zipGroupURL);
-        // console.log(zipCodeRes.data);
-        // key  ：zip_code
-        // value：縣市名
-        // member.zip_code當作key，要去對應到code.json取得縣市與區名。
-        setZipGroup(zipGroupRes.data);
-        setCities(Object.keys(zipGroupRes.data));
-        // console.log(Object.keys(zipGroupRes.data));
+        let data = zipGroupRes.data;
+        // 6.1 設定 setZipGroup 狀態，取得 group.json 所有資料。
+        setZipGroup(data);
+        // 6.2 設定 setCities 狀態，取得物件的key值，處理成各縣市的陣列。
+        setCities(Object.keys(data));
       } catch (e) {
         console.log(e);
       }
     }
     getZipGroup();
+
+    async function getZipCode() {
+      try {
+        const zipCodeRes = await axios.get(zipCodeURL);
+        let data2 = zipCodeRes.data;
+        // 6.3 設定 setZipCode 狀態，取得 code.json 所有資料。
+        setZipCode(data2);
+        console.log(data2[100].city);
+        // 6.4 設定 setCodes 狀態，取得物件的key值，處理成各個郵遞區號的陣列。
+        setCodes(Object.keys(data2));
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    getZipCode();
   }, []);
+
+  // 7. 當html select選單有onChange時，呼叫changeCity函式，此時選擇到的是唯一的值(e.target.value)，利用setCity來將原始狀態的null，改變成select到的value。
+  function changeCity(e) {
+    // 7.1 setCities是一個陣列，此處應先用setCity設定city單一值的狀態才對！
+    setCity(e.target.value);
+    // 7.2 當city有選擇時，就會自動列出該縣市的所有行政區，此時的districts也是一陣列，可利用剛剛選擇到的city作為key值，在group.json裡面找到該city的districts，並且設定到option中讓使用者選擇行政區。
+    setDistricts(zipGroup[e.target.value]);
+  }
+
+  // 8. 當實際選擇了一個行政區後，會改變此行政區狀態。不過因為郵遞區號=縣市+行政區，成為連動關係，所以一但選擇了行政區，就等於知道了郵遞區號，於是改變member資料庫之前，先將該tempMember.zip_code用setTempMember改變狀態。
+  function changeDistrict(e) {
+    setTempMember({ ...tempMember, [e.target.name]: e.target.value });
+  }
+
+  // useEffect(() => {
+  // }, [cities]);
+
   return (
     <>
       <div className="container">
@@ -103,10 +157,7 @@ function MemberEdit() {
                 </tr>
                 <tr className="member-table-active">
                   <td scope="row" className="text-center">
-                    <Link
-                      to="/member/personal"
-                      className="member-left-href-color"
-                    >
+                    <Link to="/member/edit" className="member-left-href-color">
                       帳號設定
                     </Link>
                   </td>
@@ -125,88 +176,125 @@ function MemberEdit() {
           {/* <!-- manage-right-side start--> */}
           <div className="col-12 col-lg-9 mt-5 zindex-low">
             <h2 className="member-personal-title-main">我的會員資料</h2>
-            <div className="member-personal-right-side my-4">
-              <table className="table table-borderless m-5 p-md-4 p-lg-5">
-                <tbody>
-                  <tr className="col-xs-2">
-                    <th className="member-personal-text-weight-bold">
-                      <label for="inputName">姓名：</label>
-                    </th>
-                    <td scope="row" className="member-personal-text-weight">
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="inputName"
-                        name="name"
-                        placeholder="請輸入收件人姓名"
-                        value={tempMember && tempMember.name}
-                        onChange={handleChange}
-                      />
-                    </td>
-                  </tr>
-                  <tr>
-                    <th className="member-personal-text-weight-bold">
-                      <label for="inputName">電話：</label>
-                    </th>
-                    <td scope="row" className="member-personal-text-weight">
-                      <input
-                        type="text"
-                        className="form-control mt-3"
-                        id="inputPhone"
-                        name="phone"
-                        placeholder="請輸入聯絡電話"
-                        value={tempMember && tempMember.phone}
-                        onChange={handleChange}
-                      />
-                    </td>
-                  </tr>
-                  <tr>
-                    <th className="member-personal-text-weight-bold">
-                      <label for="inputName">生日：</label>
-                    </th>
-                    <td scope="row" className="member-personal-text-weight">
-                      <input
-                        type="date"
-                        className="form-control"
-                        id="inputBirth"
-                      />
-                    </td>
-                  </tr>
-                  <tr>
-                    <th className="member-personal-text-weight-bold">
-                      <label for="inputName">地址：</label>
-                    </th>
-                    <td scope="row" className="member-personal-text-weight">
-                      {cities.map((city) => city)}
-                      <select class="form-control">
-                        <option value="">請選擇縣市</option>
-                      </select>
-                      <select class="form-control">
-                        <option value="">請選擇行政區</option>
-                      </select>
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="inputAddress"
-                        placeholder="請輸入地址"
-                      />
-                    </td>
-                  </tr>
-                  <tr>
-                    <th className="member-personal-text-weight-bold">
-                      <label for="inputName">帳號：</label>
-                    </th>
-                    <td scope="row" className="member-personal-text-weight">
-                      <input
-                        type="email"
-                        className="form-control"
-                        id="inputEmail2"
-                        placeholder="請輸入您的email"
-                      />
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+            <div className="member-personal-right-side mt-5">
+              <div className="m-4">
+                <div className="member-personal-text-weight-bold">
+                  <label for="inputName" className="mt-2">
+                    姓名：
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="inputName"
+                    name="name"
+                    placeholder="請輸入收件人姓名"
+                    value={tempMember && tempMember.name}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div className="member-personal-text-weight-bold">
+                  <label for="inputPhone" className="mt-3">
+                    電話：
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="inputPhone"
+                    name="phone"
+                    placeholder="請輸入聯絡電話"
+                    value={tempMember && tempMember.phone}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div className="member-personal-text-weight-bold">
+                  <label for="inputBirth" className="mt-3">
+                    生日：
+                  </label>
+                  <input
+                    type="date"
+                    className="form-control"
+                    id="inputBirth"
+                    name="birth"
+                    value={tempMember && tempMember.birthday}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div className="member-personal-text-weight-bold">
+                  <label for="inputAddress" className="mt-3">
+                    地址：
+                  </label>
+                  {/* 請選擇縣市 */}
+                  <select
+                    className="form-control"
+                    name="city"
+                    value={city}
+                    onChange={changeCity}
+                  >
+                    {cities &&
+                      cities.map((city, i) => (
+                        <option key={i} value={city}>
+                          {city}
+                        </option>
+                      ))}
+                  </select>
+
+                  {/* 請選擇行政區 */}
+                  <select
+                    className="form-control"
+                    value={tempMember && tempMember.zip_code}
+                    onChange={changeDistrict}
+                    name="zip_code"
+                  >
+                    {cities &&
+                      districts &&
+                      districts.map((item, i) => (
+                        <option key={i} value={item.zip_code}>
+                          {item.district}
+                        </option>
+                      ))}
+                  </select>
+                  {/* 請選擇地址 */}
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="inputAddress"
+                    placeholder="請輸入地址"
+                    name="address"
+                    value={tempMember && tempMember.addr}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div className="member-personal-text-weight-bold">
+                  <label for="inputAccount" className="mt-3">
+                    帳號：
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="inputAccount"
+                    placeholder="請輸入您的email"
+                    name="account"
+                    value={tempMember && tempMember.account}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div className="member-personal-text-weight-bold">
+                  <label for="inputPassword" className="mt-3">
+                    密碼：
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="inputPassword"
+                    name="password"
+                  />
+                </div>
+              </div>
             </div>
             <div className="border-bottom-left-radius my-5 mx-3 text-right">
               <Link type="button" className="btn btn-primary" to="">
--- mountain-react/src/components/MemberPage/MemberEdit.js
+++ mountain-react/src/components/MemberPage/MemberEdit.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'; //a標籤要變成link
 import { withRouter } from 'react-router-dom'; //可以獲取history,location,match,來使用
 import $ from 'jquery';
 import '../../styles/MemberPage/MemberPersonal.scss'; //member product and article style
-import { useAuth } from '../../context/auth';
-import { zipCodeURL, zipGroupURL } from '../../utils/config';
+import { useAuth } from '../../context/auth'; // 取得會員資料
+import { zipCodeURL, zipGroupURL } from '../../utils/config'; // 取得郵遞區號資料
 import axios from 'axios';
 
 //====== below pages star ======//
@@ -12,21 +12,24 @@ import MemberSideHead from './pages/MemberSideHead'; //member Side Head
 //====== below pages end ======//
 
 function MemberEdit() {
+  // 1. 首先，建立好 html 在 return(<>...</>)。
+  // 2. 設定狀態，關於共用會員資料使用useAuth()，關於地址資料放在靜態檔案中則使用useState()。
   const { member } = useAuth();
   const [zipGroup, setZipGroup] = useState(null);
-  const [cities, setCities] = useState([]);
-  const [city, setCity] = useState(null);
-  const [districts, setDistricts] = useState([]);
-  // const [zipCode, setZipCode] = useState(null);
+  const [zipCode, setZipCode] = useState(null);
+  const [cities, setCities] = useState([]); // 各縣市陣列
+  const [districts, setDistricts] = useState([]); //各行政區陣列
 
-  //  當member改變的時候，要去setTempMember
+  // 3. 因為不能直接去改動member的資料，需要先設定一個tempMember變數，將由資料庫而來的member放進setTempMember中改變狀態，最後才會把改變後的狀態存進資料庫。
   const [tempMember, setTempMember] = useState(null);
+  // 4. 當member有資料時，就會使用useEffect，以setTempMember改變狀態(原為null)。
   useEffect(() => {
     if (member !== null) {
       setTempMember({ ...member });
     }
   }, [member]);
 
+  // 5. 填入原始member資料後，當Html input欄位有輸入變動時，onChange呼叫handleChange函式，將react的變數tempMember，轉換成html上偵測的變數與其值([e.target.name]: e.target.value)，其中[e.target.name]為key。
   function handleChange(e) {
     // console.log(e.target.name, e.target.value);
     // console.log('onChange', e.target.name, e.target.value);
@@ -34,52 +37,94 @@ function MemberEdit() {
     // console.log('onChange After');
   }
 
-  useEffect(() => {
-    // console.log('useEffect for tempMember');
-  }, [tempMember]);
-
-  // 還沒input之前，出現兩次useEffect for tempMember：
+  // 還沒input之前，出現兩次useEffect for tempMember，結果如下：
   // useEffect for tempMember
   // useEffect for tempMember
+  // 是因為第一次render原本是空的，第二次render才有東西。
 
-  // 輸入input之後(使input欄位有變動)：
+  // 輸入input之後(使input欄位有變動)，結果如下：
   // onChange name 王大明1
   // onChange After
   // useEffect for tempMember
+  // 因為setXXX是非同步且Single Thread，所以會把setTempMember丟給quere，先繼續下一行console.log('useEffect for tempMember');
 
+  // 6. 從靜態檔案抓資料，取得 group.json 與 code.json 地址相關資料。
   useEffect(() => {
-    // 從靜態檔案抓資料
     async function getZipGroup() {
       try {
         const zipGroupRes = await axios.get(zipGroupURL);
         let data = zipGroupRes.data;
-        // console.log(data);
-        // key  ：zip_code
-        // value：縣市名
-        // member.zip_code當作key，要去對應到code.json取得縣市與區名。
+        // 6.1 設定 setZipGroup 狀態，取得 group.json 所有資料。
         setZipGroup(data);
+        // 6.2 設定 setCities 狀態，取得物件的key值，處理成各縣市的陣列。
         setCities(Object.keys(data));
-        setDistricts(Object.values(data));
-        // console.log(Object.values(data));
       } catch (e) {
         console.log(e);
       }
     }
     getZipGroup();
+
+    async function getZipCode() {
+      try {
+        const zipCodeRes = await axios.get(zipCodeURL);
+        let data2 = zipCodeRes.data;
+        // 6.3 設定 setZipCode 狀態，取得 code.json 所有資料。
+        setZipCode(data2);
+        // console.log(data2[100].city);
+        // console.log(data2[tempMember.zip_code].city);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    getZipCode();
   }, []);
 
+  useEffect(() => {
+    if (tempMember && zipCode && zipGroup) {
+      // 表示上述資料都已經有了！
+      if (tempMember.zip_code) {
+        // 表示這個使用者的 zip code 已經設定過了
+        // 城市的選單已經透過 value 的綁定處理好
+        // 這時候要處理的是 districts
+        setDistricts(zipGroup[zipCode[tempMember.zip_code].city]);
+      } else {
+        // tempMember 沒有 zip_code
+        // 想幫 tempMember 設定一個預設值
+        // 第 0 個城市的第 0 個行政區
+        // cities : ["台北市", "基隆市"]
+        // cities[0] 台北市
+        // zipGroup["台北市"][0] => {}
+        setTempMember({
+          ...tempMember,
+          zip_code: zipGroup[cities[0]][0].zip_code,
+        });
+      }
+    }
+  }, [tempMember, zipCode, zipGroup, cities]);
+
+  // 7. 當html select選單有onChange時，呼叫changeCity函式，此時選擇到的是唯一的值(e.target.value)，利用setCity來將原始狀態的null，改變成select到的value。
   function changeCity(e) {
-    setCity(e.target.value);
+    // 7.1 setCities是一個陣列，此處應先用setCity設定city單一值的狀態才對！
+    // 7.2 當city有選擇時，就會自動列出該縣市的所有行政區，此時的districts也是一陣列，可利用剛剛選擇到的city作為key值，在group.json裡面找到該city的districts，並且設定到option中讓使用者選擇行政區。
     setDistricts(zipGroup[e.target.value]);
+
+    // 預選好這組行政區中的第一個
+    setTempMember({
+      ...tempMember,
+      zip_code: zipGroup[e.target.value][0].zip_code,
+    });
   }
 
+  // 8. 當實際選擇了一個行政區後，會改變此行政區狀態。不過因為郵遞區號=縣市+行政區，成為連動關係，所以一但選擇了行政區，就等於知道了郵遞區號，於是改變member資料庫之前，先將該tempMember.zip_code用setTempMember改變狀態。
   function changeDistrict(e) {
-    // setZipCode(e.target.value);
     setTempMember({ ...tempMember, [e.target.name]: e.target.value });
   }
 
   // useEffect(() => {
   // }, [cities]);
+
+  // useEffect(() => {
+  // }, [code]);
 
   return (
     <>
@@ -136,10 +181,7 @@ function MemberEdit() {
                 </tr>
                 <tr className="member-table-active">
                   <td scope="row" className="text-center">
-                    <Link
-                      to="/member/personal"
-                      className="member-left-href-color"
-                    >
+                    <Link to="/member/edit" className="member-left-href-color">
                       帳號設定
                     </Link>
                   </td>
@@ -194,7 +236,14 @@ function MemberEdit() {
                   <label for="inputBirth" className="mt-3">
                     生日：
                   </label>
-                  <input type="date" className="form-control" id="inputBirth" />
+                  <input
+                    type="date"
+                    className="form-control"
+                    id="inputBirth"
+                    name="birthday"
+                    value={tempMember && tempMember.birthday}
+                    onChange={handleChange}
+                  />
                 </div>
 
                 <div className="member-personal-text-weight-bold">
@@ -205,7 +254,12 @@ function MemberEdit() {
                   <select
                     className="form-control"
                     name="city"
-                    value={city}
+                    value={
+                      tempMember &&
+                      zipCode &&
+                      tempMember.zip_code &&
+                      zipCode[tempMember.zip_code].city
+                    }
                     onChange={changeCity}
                   >
                     {cities &&
@@ -237,6 +291,9 @@ function MemberEdit() {
                     className="form-control"
                     id="inputAddress"
                     placeholder="請輸入地址"
+                    name="addr"
+                    value={tempMember && tempMember.addr}
+                    onChange={handleChange}
                   />
                 </div>
 
@@ -249,6 +306,9 @@ function MemberEdit() {
                     className="form-control"
                     id="inputAccount"
                     placeholder="請輸入您的email"
+                    name="account"
+                    value={tempMember && tempMember.account}
+                    onChange={handleChange}
                   />
                 </div>
 
@@ -260,6 +320,7 @@ function MemberEdit() {
                     type="password"
                     className="form-control"
                     id="inputPassword"
+                    name="password"
                   />
                 </div>
               </div>
