a787dafc4f7ed66de4abf14c19df794b7cb242a4
abbychen0427 <abbychen0427@gmail.com>
2021-09-29 23:51:08 +0800

    把console.log關掉

--- mountain-react/src/components/Navbar.js
+++ mountain-react/src/components/Navbar.js
@@ -35,7 +35,7 @@ function Navbar(props) {
   function getCartFromLocalStorage() {
     const ProductOrder =
       JSON.parse(localStorage.getItem('ProductOrderDetail')) || '[]';
-    console.log('ProductOrder', ProductOrder);
+    // console.log('ProductOrder', ProductOrder);
     setNavbarLocalCart(ProductOrder);
   }
   //一進畫面先讀取local storage
@@ -48,16 +48,16 @@ function Navbar(props) {
     // setNavbarLocalCart(navbarProductOrder);
     // console.log('navbarProductOrder', navbarProductOrder);
     // console.log('navbarProductOrder length', navbarProductOrder.length);
-    console.log('navbarProductOrder', navbarProductOrder);
+    // console.log('navbarProductOrder', navbarProductOrder);
     //判斷localstorage裡有沒有商品
     let itemsInsideCart = Boolean(
       navbarProductOrder !== '[]' && navbarProductOrder.length !== 0
     );
-    console.log('itemsInsideCart', itemsInsideCart);
+    // console.log('itemsInsideCart', itemsInsideCart);
     //if有商品
     if (itemsInsideCart) {
       $('.cart-num').css('display', 'block');
-      console.log('hello im here');
+      // console.log('hello im here');
       var totalNum = 0;
       for (let i = 0; i < navbarProductOrder.length; i++) {
         totalNum += navbarProductOrder[i].num;
@@ -68,7 +68,7 @@ function Navbar(props) {
       }
     } else {
       $('.cart-num').css('display', 'none');
-      console.log('no product');
+      // console.log('no product');
     }
     if (cartChange === false) {
       return;
