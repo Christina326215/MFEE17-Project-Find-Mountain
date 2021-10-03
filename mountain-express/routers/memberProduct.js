7b675740d66f7187e4aaa1be84ecda3e930f7428
abbychen0427 <abbychen0427@gmail.com>
2021-10-04 01:37:03 +0800

    稍微修改抓收藏商品的router

--- mountain-express/routers/memberProduct.js
+++ mountain-express/routers/memberProduct.js
@@ -2,12 +2,9 @@ const express = require("express");
 const router = express.Router();
 const connection = require("../utils/db");
 
-router.get("", async function (req, res, next) {
-  let dbResults = await connection.queryAsync("SELECT product.name AS product_name, product.price AS product_price, product.pic AS product_pic, user_product.* FROM user_product JOIN product ON user_product.product_id = product.id"); // 等資料庫查詢資料
+router.post("/", async function(req, res, next) {
+  let dbResults = await connection.queryAsync("SELECT * FROM user_product JOIN product ON user_product.product_id = product.id JOIN product_brand_name ON user_product.product_id = product_brand_name.id WHERE user_id=?", [[req.body.member.id]]);
   res.json(dbResults);
-});
+})
 
 module.exports = router;
-
-// SELECT * FROM user_article WHERE user_id = 1
-
