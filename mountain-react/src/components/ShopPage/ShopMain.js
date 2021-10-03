e0bb6a070e6d165b3d06ab7d2c7de255f1a4a73d
abbychen0427 <abbychen0427@gmail.com>
2021-10-03 16:46:17 +0800

    熱門文章排行完成

--- mountain-react/src/components/ShopPage/ShopMain.js
+++ mountain-react/src/components/ShopPage/ShopMain.js
@@ -9,6 +9,7 @@ import '../../styles/product.css';
 // import component
 import RankingItems from './components/RankingItems';
 import SelectedItems from './components/SelectedItems';
+import RankingArticles from './components/RankingArticles';
 
 import display1 from '../../img/display-photo1.jpeg';
 import display2 from '../../img/display-photo2.jpeg';
@@ -17,7 +18,6 @@ import xiangshan from '../../img/article-img/xiangshan.jpeg';
 import Yangmingshan from '../../img/article-img/Yangmingshan.jpeg';
 import Tapachien from '../../img/article-img/Tapachien.jpeg';
 
-//FIXME: 會無法slick
 function ShopMain(props) {
   useEffect(() => {
     //slick
@@ -100,106 +100,7 @@ function ShopMain(props) {
           <RankingItems />
           {/* <!-- =========熱銷不敗(本月暢銷排行) end========= --> */}
           {/* <!-- =========熱門登山攻略 start========= --> */}
-          <div>
-            <div className="position-relative shopmain-title-box">
-              <h3 className="shopmain-selected-title text-center">
-                熱門登山攻略
-              </h3>
-              <div className="shopmain-title-underline position-absolute"></div>
-            </div>
-            <div className="row my-4">
-              {/* <!--象山親山步道--> */}
-              <div className="col-lg-4 px-0">
-                <div className="shopmain-article-card">
-                  <div className="shopmain-article-img-box">
-                    <Link to="/recommend/detail/1">
-                      <img
-                        className="shopmain-cover-fit"
-                        src={xiangshan}
-                        alt="象山親山步道"
-                        title="象山親山步道"
-                      />
-                    </Link>
-                  </div>
-                  <Link
-                    to="/recommend/detail/1"
-                    className="shopmain-article-name"
-                  >
-                    象山親山步道
-                  </Link>
-                  <br />
-                  <p className="text-right">
-                    <Link
-                      to="/recommend/detail/1"
-                      className="shopmain-see-more-btn"
-                    >
-                      查看更多
-                    </Link>
-                  </p>
-                </div>
-              </div>
-              {/* <!--陽明山東西大縱走--> */}
-              <div className="col-lg-4 px-0">
-                <div className="shopmain-article-card">
-                  <div className="shopmain-article-img-box">
-                    <Link to="/recommend/detail/2">
-                      <img
-                        className="shopmain-cover-fit"
-                        src={Yangmingshan}
-                        alt="陽明山東西大縱走"
-                        title="陽明山東西大縱走"
-                      />
-                    </Link>
-                  </div>
-                  <Link
-                    to="/recommend/detail/2"
-                    className="shopmain-article-name"
-                  >
-                    陽明山東西大縱走
-                  </Link>
-                  <br />
-                  <p className="text-right">
-                    <Link
-                      to="/recommend/detail/2"
-                      className="shopmain-see-more-btn"
-                    >
-                      查看更多
-                    </Link>
-                  </p>
-                </div>
-              </div>
-              {/* <!--大霸北稜線--> */}
-              <div className="col-lg-4 px-0">
-                <div className="shopmain-article-card">
-                  <div className="shopmain-article-img-box">
-                    <Link to="/recommend/detail/3">
-                      <img
-                        className="shopmain-cover-fit"
-                        src={Tapachien}
-                        alt="大霸北稜線"
-                        title="大霸北稜線"
-                      />
-                    </Link>
-                  </div>
-                  <Link
-                    to="/recommend/detail/3"
-                    className="shopmain-article-name"
-                  >
-                    大霸北稜線
-                  </Link>
-                  <br />
-                  <p className="text-right">
-                    <Link
-                      to="/recommend/detail/3"
-                      className="shopmain-see-more-btn"
-                    >
-                      查看更多
-                    </Link>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <RankingArticles />
           {/* <!-- =========熱門登山攻略 end========= --> */}
         </div>
       </main>
