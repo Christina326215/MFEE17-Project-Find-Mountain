10cfc33836865451a1dc4b1f6a4e5145bdf96132
ouyangyuro <def2446@gmail.com>
2021-10-05 11:21:27 +0800

    10/5 會員加了spinner緩衝畫面,會員編輯資料可以改密碼了,剩改了密碼幫他登出及再次確認密碼

--- mountain-express/routers/memberEdit.js
+++ mountain-express/routers/memberEdit.js
@@ -4,8 +4,13 @@ const connection = require("../utils/db");
 // const path = require("path");
 const multer = require("multer");
 const upload = multer();
+//加密 bycypt.hash(明文,salt)
+const bcrypt = require('bcrypt')
 
 router.post("", upload.none(),  async function (req, res, next) {
+  // console.log('req.body.password:',req.body.password); //for check
+  let hashPassword = await bcrypt.hash(req.body.password, 10); // 密碼加密
+  // console.log('hashPassword',hashPassword); //for check
     // let result = await connection.queryAsync(
     //     "UPDATE user SET ? WHERE id=?", [{
     //       name : req.body.name,
@@ -20,6 +25,7 @@ router.post("", upload.none(),  async function (req, res, next) {
       "UPDATE user SET ? WHERE id=?", [{
         name : req.body.name,
         account: req.body.account,
+        password: hashPassword,
         phone: req.body.phone,
         birthday: req.body.birthday,
         zip_code: req.body.zip_code, 
