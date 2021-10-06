b2cebfe283945dd6d283c1e99d56f19643bd0ccb
abbychen0427 <abbychen0427@gmail.com>
2021-10-06 10:56:21 +0800

    拔掉搜尋欄

--- mountain-react/src/components/ShopPage/ShopMain.js
+++ mountain-react/src/components/ShopPage/ShopMain.js
@@ -32,15 +32,6 @@ function ShopMain(props) {
       <main>
         <div className="container">
           {/* <!-- =========search bar start========= --> */}
-          <form className="form my-4">
-            <div className="form-row justify-content-center">
-              <input
-                type="search"
-                placeholder="搜尋"
-                className="form-control search-bar col-5"
-              />
-            </div>
-          </form>
           {/* <!-- =========search bar end========= --> */}
           {/* <!-- =========category bar start========= --> */}
           <div className="shopmain-category-bar">
